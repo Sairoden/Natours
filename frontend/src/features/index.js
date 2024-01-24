c47626a5d38937ed36ca2addbd437cc583d9f729
Sairoden <odingandarosa@gmail.com>
2024-01-24 20:02:50 +0800

    add frontend

--- frontend/src/features/index.js
+++
@@ -1,9 +0,0 @@
-// HOME
-import Hero from "./Home/Hero";
-import About from "./Home/About";
-import Features from "./Home/Features";
-import Tours from "./Home/Tours";
-import Story from "./Home/Story";
-import Book from "./Home/Book";
-
-export { Hero, About, Features, Tours, Story, Book };
--- frontend/src/pages/index.js
+++
@@ -1,4 +0,0 @@
-import AppLayout from "./AppLayout";
-import Home from "./Home";
-
-export { AppLayout, Home };
